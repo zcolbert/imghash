3c7f6c71a9c43f8011779078115e2c9f6fcb0dd9
Zac Colbert <zcolbert1993@gmail.com>
2021-07-23 13:23:10 -0700

    Implement ahash function

--- imghash.c
+++ imghash.c
@@ -11,22 +11,6 @@ float average(int *values, size_t len)
     return (float)sum / (float)len;
 }
 
-uint64_t avg_hash(int *values, size_t len)
-{
-    float avg = average(values, len);
-    uint64_t hashval = 0;
-    uint64_t mask = pow(2.0, 63.0);
-
-    for (int i=0; i<len; ++i)
-    {
-        if (values[i] < avg) 
-        {
-            hashval |= mask;
-        }
-        mask >>= 1;
-    }
-    return hashval;
-}
 
 void resize(VipsImage *img, VipsImage *out, size_t size)
 {
@@ -58,21 +42,36 @@ void pixel_values(VipsImage *img, int *out_arr, size_t rows, size_t cols)
     g_object_unref(region);
 }
 
-uint64_t phash(VipsImage *img, size_t size)
+uint64_t ahash(VipsImage *img)
 {
     VipsImage *tmp;
 
     vips_resize(
         img, &tmp, 
-        scale_factor(size, vips_image_get_width(img)),              /* hscale */
-        "vscale", scale_factor(size, vips_image_get_height(img)),   /* vscale */
+        scale_factor(HASH_PX_PER_ROW, vips_image_get_width(img)),              /* hscale */
+        "vscale", scale_factor(HASH_NUM_OF_ROWS, vips_image_get_height(img)),   /* vscale */
         NULL
     );
     vips_colourspace(img, &tmp, VIPS_INTERPRETATION_B_W, NULL);     /* convert to greyscale */
 
-    int values[size*size];
-    pixel_values(tmp, values, size, size);
-    return avg_hash(values, size*size);
+    int values[HASH_SIZE];
+    pixel_values(tmp, values, HASH_NUM_OF_ROWS, HASH_PX_PER_ROW); 
+
+    float avg = average(values, HASH_SIZE);
+
+    /* TODO factor into a helper function */
+    uint64_t hashval = 0;
+    uint64_t mask = pow(2.0, 63.0);
+
+    for (int i=0; i<HASH_SIZE; ++i)
+    {
+        if (values[i] < avg) 
+        {
+            hashval |= mask;
+        }
+        mask >>= 1;
+    }
+    return hashval;
 }
 double scale_factor(int dim_new, int dim_current)
 {

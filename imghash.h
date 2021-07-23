5455301231c1a1f23039f90303083a78c72d8664
Zac Colbert <zcolbert1993@gmail.com>
2021-07-23 13:22:56 -0700

    Create prototypes for different hashing methods

--- imghash.h
+++ imghash.h
@@ -5,15 +5,19 @@
 #include <vips/vips.h>
 
 
+#define HASH_PX_PER_ROW 8
+#define HASH_NUM_OF_ROWS 8
+#define HASH_SIZE HASH_PX_PER_ROW*HASH_NUM_OF_ROWS
+
+
 float average(int *values, size_t len);
 double scale_factor(int dim_new, int dim_current);
 void pixel_values(VipsImage* img, int* out_arr, size_t rows, size_t cols);
 void resize(VipsImage *img, VipsImage *out, size_t size);
 
-uint64_t ahash(VipsImage *img, size_t size);
-uint64_t dhash(VipsImage *img, size_t size);
-uint64_t phash(VipsImage *img, size_t size);
-uint64_t imghash();
+uint64_t ahash(VipsImage *img);
+uint64_t dhash(VipsImage *img);
+uint64_t phash(VipsImage *img);
 
 
 #endif  // IMGHASH_H

7ee41789b1c3cb3834f36155af4eaec184411672
Zac Colbert <zcolbert1993@gmail.com>
2021-07-23 13:38:47 -0700

    Remove unused function

--- imghash.h
+++ imghash.h
@@ -13,7 +13,6 @@
 float average(int *values, size_t len);
 double scale_factor(int dim_new, int dim_current);
 void pixel_values(VipsImage* img, int* out_arr, size_t rows, size_t cols);
-void resize(VipsImage *img, VipsImage *out, size_t size);
 
 uint64_t ahash(VipsImage *img);
 uint64_t dhash(VipsImage *img);

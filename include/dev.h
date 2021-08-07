91badf23cb36a018f37022b5e46ac872f4e9c743
Zac Colbert <zcolbert1993@gmail.com>
2021-08-06 23:34:02 -0700

    Add defines for terminal color codes

--- include/dev.h
+++ include/dev.h
@@ -7,6 +7,14 @@
 #include <stdint.h>
 
 
+#define BLACK   "\033[0m"
+#define RED     "\033[1;31m"
+#define GREEN   "\033[1;32m"
+#define YELLOW  "\033[1;33m"
+#define BLUE    "\033[1;34m"
+#define CYAN    "\033[1;36m"
+
+
 void listdir(char *path);
 void listdir_by_type(char *path, char *type);
 void print_array(int *arr_vals, size_t len);
